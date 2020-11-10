8a2f364cf9029116a95efdc8ea22bf29d6324351
TeamTBD-Stevens <62127144+TeamTBD-Stevens@users.noreply.github.com>
2020-11-10 00:31:44 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,38 @@
+
+FE 595 Midterm
+
+
+Summary
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+A collaborative project between group members Yuwen Jin, Minghao Kang, Fangchi Wu, and Shiraz Bheda. The goal is to create a web interface from which the user may select up to 8 unique NLP analytical tools to be applied on a text string that is entered into a blank text box by the user:
+
+[![Screenshot-2020-11-09-210040.png](https://i.postimg.cc/Ss0w8FNk/Screenshot-2020-11-09-210040.png)](https://postimg.cc/VSRGQhCV)
+
+
+Description of NLP tools
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+1. Sentiment Analysis: indicates whether sentiment in the text string is positive or negative
+2. Language Detect: returns the name of the most possible language that the string is written in
+3. Tokenize: tokenizes the string
+4. Top Ten: returns a list of up to ten of the most frequently used words
+5. Part of Speech Tagging: assigns each word in the string to a part of speech
+6. English to French: translates the string from English to French
+7. English to Chinese: translates the string from English to Chinese
+8. Spell Check: returns spell check result of the text
+
+
+Instructions
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+One way is to run 'flask api.py' in this project directly. In this case, if you're using Windows system the homepage address is '127.0.0.1:5000', and if you're yusing ios system the address is '0.0.0.0:8000'. 
+The other way is to launch your AWS instance and connect to your running instance via your terminal. After installing git and python3, and using git clone to clone the repository to your terminal,you will need to install the requirements of the flask api prior to running the script itself.
+
+On the home page, enter text in the blank text box and select method(s) via check box, click 'submit' button then you'll see the result page. 
+If the text entry is blank you'll receive a message saying 'your text is empty', while if you click 'submit' without choosing any analyzing method you'll get another message saying 'please choose at least one analyzing method'. The 'back' button can take you back to the previous page.
+
+NOTE: While most of the required packages to run this script have been placed on a 'requirements.txt' page, there is a subfolder titled 'materials' that contains several additional packages that may need manually installing by accessing the file path directory. Once this has been completed, you can uncomment lines in 'flask api.py', 'home.html' and 'result.html', then you'll see another NLP toy, an AI response machine. 
+
+
+Example output
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+[![Screenshot-2020-11-09-213940.png](https://i.postimg.cc/1XKdF8fZ/Screenshot-2020-11-09-213940.png)](https://postimg.cc/sGvKdDYn)
+
---
+++ flask api.py
@@ -0,0 +1,170 @@
+from flask import Flask, render_template, request, url_for
+import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+import string
+from translate import Translator
+import enchant
+from langdetect import detect
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ChatterBotCorpusTrainer
+# import spacy
+
+
+# spacy.load('en')
+app = Flask(__name__)
+nltk.download('averaged_perceptron_tagger')
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/result', methods=['POST'])
+def user_rec():
+    checklist = request.form.getlist('s_option')
+    sentiment, word_bag, language, highest_counts, tags, french, chinese, out_message\
+        = ['Empty block 19491001 zhrmghgcl'] * 8
+    # ai_response = 'Empty block 19491001 zhrmghgcl'
+
+    if request.method == 'POST':
+        text = request.form['message']
+        if text is None or len(text.split()) == 0:
+            return render_template('blank error.html')
+
+        if len(checklist) == 0:
+            return render_template('no choice error.html')
+
+        if 'sentiment_analysis' in checklist:
+            sentiment = sentiment_analysis()
+
+        if 'language_detect' in checklist:
+            language = language_detect()
+
+        if 'useful_words' in checklist:
+            word_bag = useful_words()
+
+        if 'top_10' in checklist:
+            highest_counts = top_10()
+
+        if 'pos_tag' in checklist:
+            tags = pos_tag()
+
+        if 'translate_French' in checklist:
+            french = translate_French()
+
+        if 'translate_Chinese' in checklist:
+            chinese = translate_Chinese()
+
+        if 'spell_check' in checklist:
+            out_message = spell_check()
+
+        # if 'AI_response' in checklist:
+        #     response = ai_response()
+
+    return render_template('result.html', prediction=sentiment, words=word_bag,
+                           top_10=highest_counts, tags=tags, french=french, chinese=chinese,
+                           check_message=out_message, language=language
+                           # , ai_response=response
+                           )
+
+
+def sentiment_analysis():
+    message = request.form['message']
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(message)
+    result = vs["compound"]
+    return result
+
+
+def language_detect():
+    language_code = pd.read_csv('./material/language_code.csv')
+    text = request.form['message']
+    code = detect(text)
+    if code in language_code['code'].values:
+        language = language_code[language_code['code'] == code].values[0][1]
+    else:
+        language = code
+    return language
+
+
+def useful_words():
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    text = request.form['message']
+    text_tokens = word_tokenize(text)
+    word_bag = [word for word in text_tokens if word not in stopwords.words()]
+    return word_bag
+
+
+def top_10():
+    sample = request.form['message']
+    voc = sample.split()
+    counts = pd.value_counts(voc).sort_values(ascending=False)
+    highest_counts = [(counts.keys()[i], counts.values[i]) for i in range(len(counts))]
+    return highest_counts
+
+
+def pos_tag():
+    sample = request.form['message']
+    tokens = [i.strip(string.punctuation) for i in sample.split(" ")]
+    tags = nltk.pos_tag(tokens)
+    return tags
+
+
+def translate_French():
+    text = request.form['message']
+    translator = Translator(to_lang="french")
+    try:
+        french = translator.translate(text)
+    except:
+        french = 'fail to translate'
+    if text == french:
+        french = 'fail to translate'
+    return french
+
+
+def translate_Chinese():
+    text = request.form['message']
+    translator = Translator(to_lang="chinese")
+    try:
+        chinese = translator.translate(text)
+    except:
+        chinese = 'fail to translate'
+    if text == chinese:
+        chinese = 'fail to translate'
+    return chinese
+
+
+def spell_check():
+    d = enchant.Dict("en_US")
+    text = request.form['message']
+    tokens = [i.strip(string.punctuation) for i in text.split(' ')]
+    judge_list = [d.check(word) for word in tokens]
+    if all(judge_list) is True:
+        out_message = 'You spell all words correct!'
+    else:
+        wrong_index = [i for i in range(len(judge_list)) if judge_list[i] is False]
+        wrong_words = [tokens[i] for i in wrong_index]
+        out_message = "Check word(s) '{0}' if this is English text.".format("', '".join(wrong_words))
+    return out_message
+
+
+# def ai_response():
+#     text = request.form['message']
+#     BankBot = ChatBot(name='BankBot',
+#                       read_only=False,
+#                       logic_adapters=["chatterbot.logic.BestMatch"],
+#                       storage_adapter="chatterbot.storage.SQLStorageAdapter")
+#     corpus_trainer = ChatterBotCorpusTrainer(BankBot)
+#     corpus_trainer.train("emotion")
+#     response = str(BankBot.get_response(text))
+#     return response
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
